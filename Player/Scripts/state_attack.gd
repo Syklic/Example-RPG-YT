af83faf4190fe3cbed975a5a354b7dee26cee01f
Syklic <113808524+Syklic@users.noreply.github.com>
2024-11-01 16:42:50 -0500

    Update animations, Added sounds
    Updated player character animations and added sounds filles to be used for different animations and interactions.

--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -1,25 +1,36 @@
-class_name State_Idle extends State
+class_name State_Attack extends State
 
+var attacking : bool = false
+
+@onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
 @onready var walk: State = $"../Walk"
-@onready var attack: State = $"../Attack"
+@onready var idle: State_Idle = $"../Idle"
 
 
 ## What happens when the player enters this State?
 func Enter() -> void:
-	player.UpdateAnimation("idle")
+	player.UpdateAnimation("attack")
+	animation_player.animation_finished.connect( EndAttack )
+	attacking = true
 	pass
 
 
 ## What happens when the player exits this State?
 func Exit() -> void:
+	animation_player.animation_finished.disconnect( EndAttack )
+	attacking = false
 	pass
 
 
 ## what happens during the _process update in this State?
 func Process( _delta : float ) -> State:
-	if player.direction != Vector2.ZERO:
-		return walk
 	player.velocity = Vector2.ZERO
+	
+	if attacking == false:
+		if player.direction == Vector2.ZERO:
+			return idle
+		else:
+			return walk
 	return null
 
 
@@ -30,6 +41,9 @@ func Physics( _delta : float ) -> State:
 
 ## What happens with the input events in this State?
 func HandleInput( _event : InputEvent ) -> State:
-	if _event.is_action_pressed("Attack"):
-		return attack
 	return null
+
+
+
+func EndAttack( _newAnimName : String ) -> void:
+	attacking = false
--- Player/Scripts/state_walk.gd
+++ Player/Scripts/state_walk.gd
@@ -3,6 +3,7 @@ class_name State_Walk extends State
 @export var move_speed : float = 100.0
 
 @onready var idle : State = $"../Idle"
+@onready var attack: State = $"../Attack"
 
 
 ## What happens when the player enters this State?
@@ -36,4 +37,6 @@ func Physics( _delta : float ) -> State:
 
 ## What happens with the input events in this State?
 func HandleInput( _event : InputEvent ) -> State:
+	if _event.is_action_pressed("Attack"):
+		return attack
 	return null
